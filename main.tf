a79916ba7a725fe40f4c06a14b4aacd2f88aef8b
Samuel Kedida <skedida@gmail.com>
2025-02-04 13:52:18 -0500

    [Resourcely Instance] AWS EC2 Instance

--- main.tf
+++ main.tf
@@ -56,3 +56,48 @@ data "aws_ami" "skedida-instance-1_SM78z4n9M7Lk2ncR" {
     values = ["hvm"]
   }
 }
+
+resource "aws_instance" "test_u2VrZYmt2eUxaZnL" {
+  ami                    = data.aws_ami.test_u2VrZYmt2eUxaZnL.id
+  instance_type          = "a1.4xlarge"
+  key_name               = "test_u2VrZYmt2eUxaZnL"
+  vpc_security_group_ids = [aws_security_group.test_u2VrZYmt2eUxaZnL_0.id]
+
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+}
+
+resource "aws_key_pair" "test_u2VrZYmt2eUxaZnL" {
+  key_name   = "test_u2VrZYmt2eUxaZnL"
+  public_key = "test"
+}
+
+resource "aws_security_group" "test_u2VrZYmt2eUxaZnL_0" {
+  name        = "Allow Ingress"
+  description = "Allow ingress from specific CIDR"
+
+  ingress {
+    description = "test"
+    from_port   = 2
+    to_port     = 3
+    protocol    = "udp"
+    cidr_blocks = []
+  }
+}
+
+data "aws_ami" "test_u2VrZYmt2eUxaZnL" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
